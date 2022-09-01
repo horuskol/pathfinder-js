86450fb085a0f9d5d43887daeb070a693d94eb77
Stuart Jones <stuart@savvywombat.com.au>
2022-09-01 17:48:08 +0930

    move to static target if path can be found

--- src/components/WorldMap.vue
+++ src/components/WorldMap.vue
@@ -11,13 +11,13 @@
 </template>
 
 <script setup>
-import {onMounted, ref} from 'vue';
+import {onMounted, nextTick, ref} from 'vue';
 
 const canvas = ref(null);
 
-const cellSize = ref(15);
-const height = ref(50);
-const width = ref(50);
+const cellSize = ref(40);
+const height = ref(10);
+const width = ref(10);
 
 const start = {x: Math.floor(Math.random() * width.value), y: Math.floor(Math.random() * height.value)};
 const end = {x: Math.floor(Math.random() * width.value), y: Math.floor(Math.random() * height.value)};
@@ -140,13 +140,19 @@ const findPath = async () => {
     while (neighbours.length > 0) {
       const next = neighbours.shift();
 
-      frontier.unshift(next);
+      if (map[next.x][next.y] === states.EMPTY) {
+        frontier.unshift(next);
 
-      map[next.x][next.y] = states.NEXT;
-      from[`${next.x}-${next.y}`] = current;
-      drawCell(next.x, next.y, states.NEXT);
+        map[next.x][next.y] = states.NEXT;
+        from[`${next.x}-${next.y}`] = current;
+        drawCell(next.x, next.y, states.NEXT);
 
-      await sleep(10);
+        await sleep(10);
+      }
+
+      if (next.x === end.x && next.y === end.y) {
+        from[`${next.x}-${next.y}`] = current;
+      }
     }
 
     if (from[`${end.x}-${end.y}`]) {
@@ -154,25 +160,58 @@ const findPath = async () => {
     }
   }
 
-  console.log('done');
+  if (from[`${end.x}-${end.y}`]) {
+    if (end.y > from[`${end.x}-${end.y}`].y) {
+      drawCell(end.x, end.y, directions.UP);
+    }
+    if (end.x > from[`${end.x}-${end.y}`].x) {
+      drawCell(end.x, end.y, directions.LEFT);
+    }
+    if (end.y < from[`${end.x}-${end.y}`].y) {
+      drawCell(end.x, end.y, directions.DOWN);
+    }
+    if (end.x < from[`${end.x}-${end.y}`].x) {
+      drawCell(end.x, end.y, directions.RIGHT);
+    }
+
+    const path = [];
+    let current = {x: end.x, y: end.y};
+    do {
+      path.push({
+        x: current.x,
+        y: current.y,
+      });
+
+      current = {
+        x: from[`${current.x}-${current.y}`].x,
+        y: from[`${current.x}-${current.y}`].y
+      };
+    } while (from[`${current.x}-${current.y}`]);
+
+    while (path.length > 0) {
+      const move = path.pop();
+      drawCell(move.x, move.y, states.START);
+      await sleep(50);
+    }
+  }
 }
 
 const getNeighbours = ({x, y}) => {
   let neighbours = [];
 
-  if (y > 0 && map[x][y - 1] === states.EMPTY) {
+  if (y > 0) {
     neighbours.push({x, y: y - 1});
   }
 
-  if (x < width.value - 1 && map[x + 1][y] === states.EMPTY) {
+  if (x < width.value - 1) {
     neighbours.push({x: x + 1, y});
   }
 
-  if (y < height.value - 1 && map[x][y + 1] === states.EMPTY) {
+  if (y < height.value - 1) {
     neighbours.push({x, y: y + 1});
   }
 
-  if (x > 0 && map[x - 1][y] === states.EMPTY) {
+  if (x > 0) {
     neighbours.push({x: x - 1, y});
   }
 
@@ -193,15 +232,17 @@ onMounted(async () => {
     [states.START]: await loadImage('./img/start.svg', cellSize.value, cellSize.value),
     [states.WALL]: await loadImage('./img/wall.svg', cellSize.value, cellSize.value),
 
-    [directions.UP]: await loadImage('./img/up.svg', cellSize.value, cellSize.value),
-    [directions.RIGHT]: await loadImage('./img/right.svg', cellSize.value, cellSize.value),
-    [directions.DOWN]: await loadImage('./img/down.svg', cellSize.value, cellSize.value),
-    [directions.LEFT]: await loadImage('./img/left.svg', cellSize.value, cellSize.value),
+    [directions.UP]: await loadImage('./img/arrows/up.svg', cellSize.value, cellSize.value),
+    [directions.RIGHT]: await loadImage('./img/arrows/right.svg', cellSize.value, cellSize.value),
+    [directions.DOWN]: await loadImage('./img/arrows/down.svg', cellSize.value, cellSize.value),
+    [directions.LEFT]: await loadImage('./img/arrows/left.svg', cellSize.value, cellSize.value),
   }
 
-  initialiseMap();
+  await nextTick(async () => {
+    initialiseMap();
 
-  await findPath();
+    await findPath();
+  });
 });
 
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
